780ae4d21b58e9176ddd069fcb60d5db9250745b
David Zindović <96695748+davidzindovic@users.noreply.github.com>
2025-10-10 10:20:31 +0200

    Update u2.py

--- RPI/u2.py
+++ RPI/u2.py
@@ -4,7 +4,7 @@ import time
 # Configure serial port
 ser = serial.Serial(
     port='/dev/serial0',  # Default UART port on GPIO 14/15
-    baudrate=9600,
+    baudrate=115200,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS,
