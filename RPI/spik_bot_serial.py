35c222928f381584e7ea0a490394684237962bb1
David Zindović <96695748+davidzindovic@users.noreply.github.com>
2025-10-16 12:13:45 +0200

    Add serial communication functions for RPI

---
+++ RPI/spik_bot_serial.py
@@ -0,0 +1,18 @@
+def serial_init():
+  ser=serial.Serial('/dev/ttyS0',115200,timeout=1.0)
+  time.sleep(3)
+  ser.reset_input_buffer()
+  print("Serial OK")
+
+def serial_transmit(message):
+  print("Sending: "+message)
+  ser.write(message.encode('utf-8'))
+
+def serial_receive():
+  print("Receiving message.")
+  x=ser.readline()
+  if len(x)>2:
+    print("Received: "+x)
+  else:
+    print("Received message is invalid")
+  ser.reset_output_buffer()
