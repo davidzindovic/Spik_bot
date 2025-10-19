dbb5ca6cb72539fba4321ffa7193414b9c6dd4a7
David Zindović <96695748+davidzindovic@users.noreply.github.com>
2025-10-19 15:02:26 +0200

    Update spik_bot_serial.py

--- RPI/spik_bot_serial.py
+++ RPI/spik_bot_serial.py
@@ -4,16 +4,19 @@ import time
 ser=serial.Serial('/dev/ttyS0',115200,timeout=1.0)
 
 def serial_init():
+  global ser
   #ser=serial.Serial('/dev/ttyS0',115200,timeout=1.0)
   time.sleep(3)
   ser.reset_input_buffer()
   print("Serial OK")
 
 def serial_transmit(message):
+  global ser
   print("Sending: "+message)
   ser.write(message.encode('utf-8'))
 
 def serial_receive():
+  global ser
   print("Receiving message...")
   x=ser.readline()
   if len(x)>2:
