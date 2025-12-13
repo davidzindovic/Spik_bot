410d911e39cb460b62cc1999bb97c01874c05196
David Zindović <96695748+davidzindovic@users.noreply.github.com>
2025-12-13 21:29:16 +0100

    Enhance serial communication in main.py
    Added serial transmission of coordinates and improved message logging.

--- RPI/main.py
+++ RPI/main.py
@@ -6,11 +6,12 @@ try:
   spik_bot_serial.serial_init()
   
   while(1):
+    spik_bot_serial.serial_transmit("X:35 Y:100")
     sporocilo=spik_bot_serial.serial_receive()
-    print(sporocilo)
-    if sporocilo is not None:
+    print("Prejel sem sporočilo: {sporocilo}")
+    #if sporocilo is not None:
       #spik_bot_serial.serial_transmit(sporocilo)
-      spik_bot_serial.serial_transmit("X:35 Y:100")
+      
 
 except KeyboardInterrupt:
   spik_bot_serial.serial_close()
