cbb21feb796befeedd7235f08aa2f822e6805015
David Zindović <96695748+davidzindovic@users.noreply.github.com>
2025-12-19 15:17:11 +0100

    Update main.py

--- RPI/main.py
+++ RPI/main.py
@@ -8,19 +8,6 @@ try:
   while(1):
     poslano="X:35 Y:100"
     spik_bot_serial.serial_transmit(poslano)
-    sporocilo=None
-    while(sporocilo is None):
-      sporocilo=spik_bot_serial.serial_receive()
-    print("Prejel sem sporočilo: ",sporocilo)
-    if sporocilo==poslano:
-      print("Uspesno!")
-      spik_bot_serial.serial_transmit("OK")
-      while(1):
-        pass
-    else:
-      print("Neuspesno!")
-      spik_bot_serial.serial_transmit("NOT OK")
-      
 
 except KeyboardInterrupt:
   spik_bot_serial.serial_close()
