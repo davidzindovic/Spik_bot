e507d6376b3c0d7856a1ed0b1b824605c87a59d7
David Zindović <96695748+davidzindovic@users.noreply.github.com>
2025-12-20 09:56:39 +0100

    Update main.py

--- RPI/main.py
+++ RPI/main.py
@@ -1,19 +1,30 @@
 import lib_init
 import spik_bot_serial
 import data_capture_and_process
+import glob
+import os
 
 try:
   lib_init.library_setup()
+
+  #odkomentiraj za test na hw:
   spik_bot_serial.serial_init()
   
   while(1):
 
-    color_images = glob.glob("*barva.png")
+    color_images = glob.glob("zajeti_podatki/*barva.png")
     
     for color_img_path in sorted(color_images):
-        img_number = color_img_path.replace("barva.png", "")
-        depth_img_path = f"{img_number}globina.npy"
-        if not os.path.exists(depth_img_path): continue
+        # Ločimo mapo in ime datoteke (npr. "zajeti_podatki/" in "1barva.png")
+        directory, filename = os.path.split(color_img_path)
+        
+        # Zamenjamo del imena datoteke
+        depth_filename = filename.replace("barva.png", "globina.npy")
+        
+        # Ponovno združimo v polno pot
+        depth_img_path = os.path.join(directory, depth_filename)
+        
+        print(f"Barvna: {color_img_path} -> Globinska: {depth_img_path}")
     
         poslano=data_capture_and_process.exec(color_img_path,depth_img_path)
         spik_bot_serial.serial_transmit(poslano)
