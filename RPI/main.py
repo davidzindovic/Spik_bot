8b33e021a7e996c3ba8f50548c2ca4fbfd6ce0b4
David Zindović <96695748+davidzindovic@users.noreply.github.com>
2025-12-20 09:20:46 +0100

    Update main.py

--- RPI/main.py
+++ RPI/main.py
@@ -6,6 +6,14 @@ try:
   spik_bot_serial.serial_init()
   
   while(1):
+
+    color_images = glob.glob("*barva.png")
+    
+    for color_img_path in sorted(color_images):
+        img_number = color_img_path.replace("barva.png", "")
+        depth_img_path = f"{img_number}globina.npy"
+        if not os.path.exists(depth_img_path): continue
+    
     poslano="X:35 Y:100"
     spik_bot_serial.serial_transmit(poslano)
 
